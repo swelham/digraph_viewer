d687dd0ed4d34f3cdc0ce98e6b2acee8a5d66101
swelham <stuart.welham@gmail.com>
2018-03-19 21:11:47 +0000

    handle non atom vertices

--- src/digraph_viewer.erl
+++ src/digraph_viewer.erl
@@ -22,14 +22,23 @@ call_server(Request) ->
    
 collect_nodes(G) ->
   Nodes = digraph:vertices(G),
-  lists:map(fun(Id) -> {[{id, Id}]} end, Nodes).
+  lists:map(fun(Id) -> 
+    IdStr = format_term(Id),
+    {[{id, IdStr}]} end, Nodes).
 
 collect_links(G) ->
   Edges = digraph:edges(G),
   lists:map(fun(E) ->
     {_, Source, Target, _} = digraph:edge(G, E),
-    {[{source, Source}, {target, Target}]}
+    Src = format_term(Source),
+    Trgt = format_term(Target),
+    {[{source, Src}, {target, Trgt}]}
   end, Edges).
 
 do_register(G, Name) ->
   gen_server:cast(graph_tracker, {register, G, Name}).
+
+format_term(Term) ->
+  R = io_lib:format("~p",[Term]),
+  L = lists:flatten(R),
+  iolist_to_binary(L).
